d0f696f98f04fa7a8e9cf11bee3530c1981685e2
Precious Tom <tomprezine@gmail.com>
2020-02-12 08:38:38 +0100

    correct pathing

--- index.php
+++ index.php
@@ -8,12 +8,12 @@
     <meta content="Yummy Bricks" name="author">
     <meta content="A game of falling bricks and points" name="description">
     <meta content="width=device-width, initial-scale=1" name="viewport">
-    <link rel="stylesheet" type="text/css" href="css/main.css">
-    <link rel="stylesheet" type="text/css" href="css/custom.css">
+    <link rel="stylesheet" type="text/css" href="./app/css/main.css">
+    <link rel="stylesheet" type="text/css" href="./app/css/custom.css">
 </head>
 <body>
 
-	<img src="img/duck.gif" class="duck">
+	<img src="./app/img/duck.gif" class="duck">
 	
 	<div class="game">
 		<div class="counter" id="counter">00s</div>
@@ -26,7 +26,7 @@
 	<div class="floor"></div>
 
 	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
-	<script src="js/main.js"></script>
-	<script src="js/engine.js"></script>
+	<script src="./app/js/main.js"></script>
+	<script src="./app/js/engine.js"></script>
 </body>
 </html>
