71e5f7f0bf48f7951be3c61542d4c3b79f2f7506
Precious Tom <tomprezine@gmail.com>
2020-02-12 08:36:51 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Yummy Bricks</title>
+	<meta charset="utf-8">
+    <meta content="ie=edge" http-equiv="x-ua-compatible">
+    <meta content="" name="keywords">
+    <meta content="Yummy Bricks" name="author">
+    <meta content="A game of falling bricks and points" name="description">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/custom.css">
+</head>
+<body>
+
+	<img src="img/duck.gif" class="duck">
+	
+	<div class="game">
+		<div class="counter" id="counter">00s</div>
+		<div class="score">
+		00
+		</div>
+	</div>
+
+	<div class="grass"></div>
+	<div class="floor"></div>
+
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+	<script src="js/main.js"></script>
+	<script src="js/engine.js"></script>
+</body>
+</html>
